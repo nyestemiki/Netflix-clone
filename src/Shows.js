29f81890dbd6ff12393228d9a0977771ce818177
nyestemiki <nyeste.miki@yahoo.com>
2019-09-22 16:48:48 +0300

    Sample movies

---
+++ src/Shows.js
@@ -0,0 +1,57 @@
+// All the movies and tv shows available in the app
+const Shows = {
+    /* 
+        Template
+        'title': {
+            displayedTitle: '',
+            cover: ''
+        }
+    */
+
+    brave: {
+        displayedTitle: "Brave",
+        cover: '/imgs/covers/brave.jpg'
+    },
+    extinction: {
+        displayedTitle: "Extinction",
+        cover: '/imgs/covers/extinction.jpg'
+    },
+    godzilla2: {
+        displayedTitle: "Godzilla 2",
+        cover: '/imgs/covers/godzilla2.jpg'
+    },
+    greenLantern: {
+        displayedTitle: "Green Lantern",
+        cover: '/imgs/covers/greenLantern.jpg'
+    },
+    johnnyEnglishReborn: {
+        displayedTitle: "Johnny English Reborn",
+        cover: '/imgs/covers/johnnyEnglishReborn.jpg'
+    },
+    starWarsBattlefront2: {
+        displayedTitle: "Star Wars: Battlefront 2",
+        cover: '/imgs/covers/starWarsBattlefront2.jpg'
+    },
+    theDarkKnight: {
+        displayedTitle: "The Dark Knight",
+        cover: '/imgs/covers/theDarkKnight.jpg'
+    },
+    theMummy: {
+        displayedTitle: "The Mummy",
+        cover: '/imgs/covers/theMummy.jpg'
+    },
+    theRevenant: {
+        displayedTitle: "The Revenant",
+        cover: '/imgs/covers/theRevenant.jpg'
+    },
+    thor: {
+        displayedTitle: "Thor",
+        cover: '/imgs/covers/thor.jpg'
+    },
+    worldWarZ: {
+        displayedTitle: "World War Z",
+        cover: '/imgs/covers/worldWarZ.jpg'
+    },
+}
+
+export default Shows;
