6b30f2f4b67af37e9bdb170b5d3883077199a5fc
nyestemiki <nyeste.miki@yahoo.com>
2019-09-24 22:09:30 +0300

    Empty account manager on hover

--- src/components/AccountMenu.js
+++ src/components/AccountMenu.js
@@ -2,37 +2,31 @@ import React, { Component } from 'react';
 import AccountMenuContainer from './styles/AccountMenuStyles';
 
 export default class AccountMenu extends Component {
-    hoversSet = false
-
     state = {
         show: false
     }
 
-    hoverListeners = () => {
-        const accountMenuDiv = document.getElementById('accountContainer');
-        accountMenuDiv.addEventListener('mouseenter', () => {
-            this.setState({ show: true });
-        });
-        accountMenuDiv.addEventListener('mouseleave', () => {
-            setTimeout(() => {
-                this.setState({ show: false });
-            }, 1000);
-        });
-        this.hoversSet = true;
-    }
-
     componentDidUpdate() {
-        if (!this.hoversSet) {
-            this.hoverListeners();
+        const accountMenuDiv = document.getElementById('accountContainer');
+
+        if (accountMenuDiv) {
+            accountMenuDiv.addEventListener('mouseenter', () => {
+                this.setState({ show: true });
+            });
+            accountMenuDiv.addEventListener('mouseleave', () => {
+                setTimeout(() => {
+                    this.setState({ show: false });
+                }, 1000);
+            });
         }
     }
 
     render() {
         return (
-            // Show menu on icon-hover or menu-hover
-            (this.state.show || this.props.show) && (
-                <AccountMenuContainer id="accountContainer" onAnimationEnd={this.hoverListeners}>
-
+            // Showing only if hovering over the account icon or this menu
+            (this.props.show || this.state.show) && (
+                <AccountMenuContainer id="accountContainer">
+                    
                 </AccountMenuContainer>
             )
         )
--- src/components/styles/AccountMenuStyles.js
+++ src/components/styles/AccountMenuStyles.js
@@ -2,13 +2,10 @@ import styled from 'styled-components';
 
 const AccountMenuContainer = styled.div`
     position: absolute;
-    width: 100px;
-    height: 100px;
-    border: 1px solid cyan;
     right: 0;
-    top: 25px;
+    top: 35px;
     margin: 40px;
-    background: black;
+    background: rgba(0, 0, 0, 0.85);
     z-index: 1000;
 `;
 
