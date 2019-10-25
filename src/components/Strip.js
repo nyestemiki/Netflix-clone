1b789fbcf6c4c18ca91ec0eb2f113bcacbf50cdd
nyestemiki <nyeste.miki@yahoo.com>
2019-10-25 14:04:25 +0300

    Removed nav-arrows in strips

--- src/components/Strip.js
+++ src/components/Strip.js
@@ -22,8 +22,6 @@ export default class Strip extends Component {
                             />
                         )}
                     </div>
-                    <button aria-label="Previous" className="glider-prev">«</button>
-                    <button aria-label="Next" className="glider-next">»</button>
                     <div role="tablist" className="dots"></div>
                 </div>
             </>
